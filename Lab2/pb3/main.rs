30ada124e545ce146af2e48ffb36f923e30b1ab8
TeodorBujorean <73654965+Amostein@users.noreply.github.com>
2024-10-17 19:25:49 +0300

    Update main.rs

--- Lab2/pb3/main.rs
+++ Lab2/pb3/main.rs
@@ -1,10 +1,10 @@
 fn add_space(s: &mut String, n: u32) {
-    let mut i=0;
+    let mut i = 0;
     let spc: &str = " ";
     while i < n {
-        i+=1;
+        i += 1;
         s.push_str(&spc);
-}
+    }
 }
 
 fn add_str(s: &mut String, after: String) {
@@ -16,20 +16,20 @@ fn add_integer(s: &mut String, x: u32) {
     let lung = numar.len();
     let mut step = 0;
     let mut ok = 0;
-    if lung %3!=0{
-        s.push_str(&numar[step..step+lung%3]);
+    if lung % 3 != 0 {
+        s.push_str(&numar[step..step + lung % 3]);
         step = step + lung % 3;
         ok = 1;
     }
 
-    while step<lung {
+    while step < lung {
         if ok == 1 {
             s.push_str("_");
         }
-        s.push_str(&numar[step..step+3]);
+        s.push_str(&numar[step..step + 3]);
         step = step + 3;
         if step < lung {
-        s.push_str("_");
+            s.push_str("_");
         }
     }
 }
@@ -46,48 +46,48 @@ fn main() {
     add_str(&mut s, (&"💚").to_string());
     println!("{}", s);
     s.clear();
-    
+
     add_space(&mut s, 38);
     add_str(&mut s, (&"RUST.").to_string());
     println!("{}", s);
     s.clear();
 
     add_str(&mut s, (&"Most").to_string());
-    add_space(&mut s, (&("downloaded").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("downloaded").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"crate").to_string());
-    add_space(&mut s, (&("has").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("has").len() + 2).try_into().unwrap());
 
-    let number=306437968;
+    let number = 306437968;
     add_integer(&mut s, number);
-    add_space(&mut s, (&("downloads").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("downloads").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"and").to_string());
-    add_space(&mut s, (&("the").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("the").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"lastest").to_string());
-    add_space(&mut s, (&("version").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("version").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"is").to_string());
     println!("{}", s);
     s.clear();
 
-    add_space(&mut s, (&("Most").len()+1).try_into().unwrap());
+    add_space(&mut s, (&("Most").len() + 1).try_into().unwrap());
 
     add_str(&mut s, (&"downloaded").to_string());
-    add_space(&mut s, (&("crate").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("crate").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"has").to_string());
-    add_space(&mut s, ((&"306_437_968").len()+2).try_into().unwrap());
+    add_space(&mut s, ((&"306_437_968").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"downloads").to_string());
-    add_space(&mut s, (&("and").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("and").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"the").to_string());
-    add_space(&mut s, (&("lastest").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("lastest").len() + 2).try_into().unwrap());
 
     add_str(&mut s, (&"version").to_string());
-    add_space(&mut s, (&("is").len()+2).try_into().unwrap());
+    add_space(&mut s, (&("is").len() + 2).try_into().unwrap());
 
     add_float(&mut s, 2.038);
     add_str(&mut s, (&".").to_string());
